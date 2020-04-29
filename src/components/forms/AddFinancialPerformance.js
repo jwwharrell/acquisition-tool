57658775f0c1fd05b7fcadf2238b1477fa833231
Jordan <jwharrell0516@gmail.com>
2020-04-29 09:55:46 -0400

    Created fade in for AddFinancialPerformance

--- src/components/forms/AddFinancialPerformance.js
+++ src/components/forms/AddFinancialPerformance.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import FadeIn from 'react-fade-in'
+
 
 export default class AddFinancialPerformance extends Component {
     state = {
@@ -79,79 +81,81 @@ export default class AddFinancialPerformance extends Component {
 
     render() {
         return (
-            <div>
-                <form
-                    onSubmit={this.createNewPerfData}
-                    className='card--form'>
-                    <input
-                        type='number'
-                        placeholder='2018 Q1 Net Profit'
-                        name="q1_2018"
-                        required="required"
-                        onChange={this.onNewPerfQ118Change}
-                        value={this.state.performance.q1_2018}
-                    />
-                    <input
-                        type='number'
-                        placeholder='2018 Q2 Net Profit'
-                        name="q2_2018"
-                        required="required"
-                        onChange={this.onNewPerfQ218Change}
-                        value={this.state.performance.q2_2018}
-                    />
-                    <input
-                        type='number'
-                        placeholder='2018 Q3 Net Profit'
-                        name="q3_2018"
-                        required="required"
-                        onChange={this.onNewPerfQ318Change}
-                        value={this.state.performance.q3_2018}
-                    />
-                    <input
-                        type='number'
-                        placeholder='2018 Q4 Net Profit'
-                        name="q4_2018"
-                        required="required"
-                        onChange={this.onNewPerfQ418Change}
-                        value={this.state.performance.q4_2018}
-                    />
-                    <input
-                        type='number'
-                        placeholder='2019 Q1 Net Profit'
-                        name="q1_2019"
-                        required="required"
-                        onChange={this.onNewPerfQ119Change}
-                        value={this.state.performance.q1_2019}
-                    />
-                    <input
-                        type='number'
-                        placeholder='2019 Q2 Net Profit'
-                        name="q2_2019"
-                        required="required"
-                        onChange={this.onNewPerfQ219Change}
-                        value={this.state.performance.q2_2019}
-                    />
-                    <input
-                        type='number'
-                        placeholder='2019 Q3 Net Profit'
-                        name="q3_2019"
-                        required="required"
-                        onChange={this.onNewPerfQ319Change}
-                        value={this.state.performance.q3_2019}
-                    />
-                    <input
-                        type='number'
-                        placeholder='2019 Q4 Net Profit'
-                        name="q4_2019"
-                        required="required"
-                        onChange={this.onNewPerfQ419Change}
-                        value={this.state.performance.q4_2019}
-                    />
-                    <input
-                        type='submit'
-                    />
-                </form>
-            </div>
+            <FadeIn>
+                <div>
+                    <form
+                        onSubmit={this.createNewPerfData}
+                        className='card--form'>
+                        <input
+                            type='number'
+                            placeholder='2018 Q1 Net Profit'
+                            name="q1_2018"
+                            required="required"
+                            onChange={this.onNewPerfQ118Change}
+                            value={this.state.performance.q1_2018}
+                        />
+                        <input
+                            type='number'
+                            placeholder='2018 Q2 Net Profit'
+                            name="q2_2018"
+                            required="required"
+                            onChange={this.onNewPerfQ218Change}
+                            value={this.state.performance.q2_2018}
+                        />
+                        <input
+                            type='number'
+                            placeholder='2018 Q3 Net Profit'
+                            name="q3_2018"
+                            required="required"
+                            onChange={this.onNewPerfQ318Change}
+                            value={this.state.performance.q3_2018}
+                        />
+                        <input
+                            type='number'
+                            placeholder='2018 Q4 Net Profit'
+                            name="q4_2018"
+                            required="required"
+                            onChange={this.onNewPerfQ418Change}
+                            value={this.state.performance.q4_2018}
+                        />
+                        <input
+                            type='number'
+                            placeholder='2019 Q1 Net Profit'
+                            name="q1_2019"
+                            required="required"
+                            onChange={this.onNewPerfQ119Change}
+                            value={this.state.performance.q1_2019}
+                        />
+                        <input
+                            type='number'
+                            placeholder='2019 Q2 Net Profit'
+                            name="q2_2019"
+                            required="required"
+                            onChange={this.onNewPerfQ219Change}
+                            value={this.state.performance.q2_2019}
+                        />
+                        <input
+                            type='number'
+                            placeholder='2019 Q3 Net Profit'
+                            name="q3_2019"
+                            required="required"
+                            onChange={this.onNewPerfQ319Change}
+                            value={this.state.performance.q3_2019}
+                        />
+                        <input
+                            type='number'
+                            placeholder='2019 Q4 Net Profit'
+                            name="q4_2019"
+                            required="required"
+                            onChange={this.onNewPerfQ419Change}
+                            value={this.state.performance.q4_2019}
+                        />
+                        <input
+                            type='submit'
+                        />
+                    </form>
+                </div>
+            </FadeIn>
         )
     }
 }
