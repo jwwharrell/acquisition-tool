3c834123ad3bcd33929910eab8c80852a347ce2e
Jordan <jwharrell0516@gmail.com>
2020-04-29 09:50:31 -0400

    Created fade in for CompanyCard

--- src/components/cards/CompanyCard.js
+++ src/components/cards/CompanyCard.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import FinancialPerformance from './FinancialPerformance.js'
 import ContactList from './ContactList.js'
 import EditTarget from '../forms/EditTarget.js'
+import FadeIn from 'react-fade-in'
 
 export default class CompanyCard extends Component {
     state = {
@@ -13,39 +14,41 @@ export default class CompanyCard extends Component {
         previousState.buttonPressed = !this.state.buttonPressed
         this.setState(previousState)
     }
-    
+
     render() {
         return (
-            <div className='company'>
-                <div className='company--header'>
-                    <h2>{this.props.targetInfo.name}</h2>
-                    <span className='statusMark'>{this.props.targetInfo.status}</span>
-                    <p>{this.props.targetInfo.info}</p>
-                    <span className='defaultBtn'
-                        onClick={this.onButtonPress}
-                    >
-                        {this.state.buttonPressed ? 'Hide Form'
-                        : 'Edit Basic Information'}
-                    </span>
-                    {this.state.buttonPressed ? 
-                    <EditTarget 
+            <FadeIn>
+                <div className='company'>
+                    <div className='company--header'>
+                        <h2>{this.props.targetInfo.name}</h2>
+                        <span className='statusMark'>{this.props.targetInfo.status}</span>
+                        <p>{this.props.targetInfo.info}</p>
+                        <span className='defaultBtn'
+                            onClick={this.onButtonPress}
+                        >
+                            {this.state.buttonPressed ? 'Hide Form'
+                                : 'Edit Basic Information'}
+                        </span>
+                        {this.state.buttonPressed ?
+                            <EditTarget
+                                targetInfo={this.props.targetInfo}
+                                onSubmitEdit={this.props.onSubmitEdit}
+                            />
+                            : null}
+                    </div>
+                    <ContactList
                         targetInfo={this.props.targetInfo}
-                        onSubmitEdit={this.props.onSubmitEdit} 
-                    /> 
-                : null}
+                        addContact={this.props.addContact}
+                        editContact={this.props.editContact}
+                        deleteContact={this.props.deleteContact}
+                        contactSearch={this.props.contactSearch}
+                    />
+                    <FinancialPerformance
+                        targetInfo={this.props.targetInfo}
+                        addFinData={this.props.addFinData}
+                    />
                 </div>
-                <ContactList 
-                    targetInfo={this.props.targetInfo}
-                    addContact={this.props.addContact}
-                    editContact={this.props.editContact}
-                    deleteContact={this.props.deleteContact}
-                    contactSearch={this.props.contactSearch}
-                />
-                <FinancialPerformance 
-                    targetInfo={this.props.targetInfo}
-                    addFinData={this.props.addFinData}
-                />
-            </div>
+            </FadeIn>
         )
     }
 }
